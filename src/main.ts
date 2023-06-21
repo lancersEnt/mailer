df962ecfe4bf3b6689a15cd0ac434932612fda30
Ghassen Saaf <saafghassen@gmail.com>
2023-06-21 19:00:12 +0100

    change port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(9669);
+  await app.listen(9449);
 }
 bootstrap();
